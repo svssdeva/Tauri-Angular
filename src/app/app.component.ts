a07c625567054424da617b8f963dab9cbf9e5441
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2023-11-30 18:02:44 +0530

    some more changes to prettier, eslintrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,19 @@ import { Component } from '@angular/core'
 import { invoke } from '@tauri-apps/api/tauri'
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    greetingMessage = ''
+  greetingMessage = ''
 
-    greet(event: SubmitEvent, name: string): void {
-        event.preventDefault()
+  greet(event: SubmitEvent, name: string): void {
+    event.preventDefault()
 
-        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        invoke<string>('greet', { name }).then((text) => {
-            this.greetingMessage = text
-        })
-    }
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    invoke<string>('greet', { name }).then(text => {
+      this.greetingMessage = text
+    })
+  }
 }
